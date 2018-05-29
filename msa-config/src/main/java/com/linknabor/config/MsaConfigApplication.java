ef16b8f6914b46e27fc6765ddc9cce4065ffae25
liuzj_jj@163.com <liuzj_jj@163.com>
2018-05-29 20:34:06 +0800

    配置服务

---
+++ msa-config/src/main/java/com/linknabor/config/MsaConfigApplication.java
@@ -0,0 +1,12 @@
+package com.linknabor.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaConfigApplication.class, args);
+	}
+}
