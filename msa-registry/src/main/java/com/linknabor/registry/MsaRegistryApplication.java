fe76402c67329f12ddaf5a3521c6cac4778a9787
liuzj_jj@163.com <liuzj_jj@163.com>
2018-05-29 20:33:46 +0800

    注册中心

---
+++ msa-registry/src/main/java/com/linknabor/registry/MsaRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.linknabor.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaRegistryApplication.class, args);
+	}
+}
