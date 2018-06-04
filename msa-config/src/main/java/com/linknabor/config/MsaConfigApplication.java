ed3cd839cac5a4fb54a79cc24bdc8d0937d8daa8
liuzj_jj@163.com <liuzj_jj@163.com>
2018-06-04 20:05:19 +0800

    config配置中心

--- msa-config/src/main/java/com/linknabor/config/MsaConfigApplication.java
+++ msa-config/src/main/java/com/linknabor/config/MsaConfigApplication.java
@@ -2,11 +2,15 @@ package com.linknabor.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class MsaConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsaConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsaConfigApplication.class, args);
+    }
 }
